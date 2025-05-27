1836671a316107bd99b0d4e4b4f77e5a4b59ce03
trong.tran <trong.tran@bnksolution.com>
2025-05-27 16:48:44 +0700

    Update feedback 27/05

--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -41,6 +41,7 @@ const ThreeScene = () => {
     scene.add(dirLight);
 
     const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableZoom = false;
     controls.enableDamping = true;
     controls.dampingFactor = 0.05;
 
@@ -57,9 +58,9 @@ const ThreeScene = () => {
     const options = [
       {
         path: '/models/ロゴ3.glb',
-        position: { x: 0, y: -10, z: -1 },
+        position: { x: 0, y: -10, z: 0 },
         scale: { x: 70, y: 70, z: 70 },
-        timeScale: 1.0,
+        timeScale: 2.0,
         cameraZoom: 5,
         renderOrder: 1,
         isLooping: true,
@@ -68,7 +69,7 @@ const ThreeScene = () => {
         path: '/models/クマ3.glb',
         position: { x: 12, y: -16, z: 0 },
         scale: { x: 50, y: 50, z: 50 },
-        timeScale: 1.0,
+        timeScale: 2.0,
         cameraZoom: 5,
         renderOrder: 2,
         isLooping: false,
@@ -77,7 +78,7 @@ const ThreeScene = () => {
         path: '/models/パソコン2.glb',
         position: { x: 1, y: -13, z: 0 },
         scale: { x: 12, y: 12, z: 12 },
-        timeScale: 1.0,
+        timeScale: 2.0,
         cameraZoom: 5,
         renderOrder: 3,
         isLooping: false,
@@ -86,7 +87,7 @@ const ThreeScene = () => {
         path: '/models/卵3.glb',
         position: { x: -15, y: -12, z: 0 },
         scale: { x: 0.8, y: 0.8, z: 0.8 },
-        timeScale: 8.0,
+        timeScale: 14.0,
         cameraZoom: 3,
         renderOrder: 4,
         isLooping: false,
